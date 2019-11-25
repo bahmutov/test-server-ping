538320203bc363dba2457bd280f05d6e7c549ab0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-25 10:14:50 -0500

    rework ping a little

--- ping.js
+++ ping.js
@@ -1,17 +1,31 @@
 const got = require('got')
 console.log('node ping')
 
-const url = 'http://localhost:5000'
-got(url, {
-  retry: {
-    retries (retry, error) {
-      console.error(error.method, error.host, error.code)
-      return 1000
+const ping = (url, timeout) => {
+  const start = +new Date()
+  return got(url, {
+    retry: {
+      retries (retry, error) {
+        const now = +new Date()
+        console.error(now - start, 'ms', error.method, error.host, error.code)
+        if (now - start > timeout) {
+          console.error('timed out')
+          return 0
+        }
+        return 1000
+      }
     }
+  })
+}
+
+const url = 'http://localhost:5000'
+const timeout = 5000
+ping(url, timeout).then(
+  () => {
+    console.log('success pinging', url)
+  },
+  e => {
+    console.error('error', e.method, e.host, e.code)
+    process.exit(1)
   }
-}).then(() => {
-  console.log('success pinging', url)
-}, (e) => {
-  console.error('error')
-  console.error(e)
-})
+)
